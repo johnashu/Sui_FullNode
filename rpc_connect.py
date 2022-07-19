977042b9e667856fee2941ea63aee8bf2e962d4f
johnashurst-sg <73337262+johnashurst-sg@users.noreply.github.com>
2022-07-19 12:37:24 +0200

    [UPD] readme

--- rpc_connect.py
+++ rpc_connect.py
@@ -1,10 +1,6 @@
-import json
-from urllib import response
-from brownie import rpc
 import requests
 from includes.config import *
 from tools import file_op
-import pysui.rpc.request
 
 
 rpc_url = "http://154.53.59.19:9000"
@@ -34,14 +30,12 @@ r = (
 
 for json_data in r:
     log.info(json_data["method"])
-    # response = requests.post(rpc_url, headers=headers, json=json_data)
-    response = pysui.rpc.request.rpc_request(
-        json_data["method"], json_data.get("params"), endpoint=rpc_url
-    )
+    response = requests.post(rpc_url, headers=headers, json=json_data)
+
     # print(response.text)
     try:
-        data = response
+        data = response.json()
     except Exception as e:
-        data = response
+        data = response.text
     # log.info(data)
     file_op.save_json(join(json_out, json_data["method"] + "_rpc"), data)
